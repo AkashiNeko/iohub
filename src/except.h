0121e8b06b8adc0c2eb446e80adebde6b9fdb25f
AkashiNeko <akashineko@qq.com>
2024-02-01 20:18:59 +0800

    add LICENSE

--- src/except.h
+++ src/except.h
@@ -1,8 +1,32 @@
-// except.h
+// File:     src/except.h
+// Author:   AkashiNeko
+// Project:  iohub
+// Github:   https://github.com/AkashiNeko/iohub/
+
+/* Copyright AkashiNeko. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #pragma once
-#ifndef IOHUB_EXCEPT_H_
-#define IOHUB_EXCEPT_H_
+#ifndef IOHUB_EXCEPT_H
+#define IOHUB_EXCEPT_H
 
 
 // C
@@ -59,4 +83,4 @@ inline void assert_throw(bool condition, const Args&... args) {
 
 } // namespace iohub
 
-#endif // IOHUB_EXCEPT_H_
+#endif // IOHUB_EXCEPT_H
